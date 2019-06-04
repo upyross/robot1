9ddd75ae6aac551d0f0dd6f780fff7ce4ebfc78f
ross <dev@rross.me>
2019-06-04 14:40:14 -0500

    Body and right wheel added

---
+++ controllers/robot1/build/release/robot1.d
@@ -0,0 +1,5 @@
+build/release/robot1.o: robot1.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h
---
+++ controllers/robot1/robot1.c
@@ -0,0 +1,71 @@
+/*
+ * File:          robot1.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/differential_wheels.h>, etc.
+ */
+#include <webots/robot.h>
+#include <webots/motor.h>
+
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv)
+{
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+
+  /*
+   * You should declare here WbDeviceTag variables for storing
+   * robot devices like this:
+   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+   */
+  WbDeviceTag motor_r = wb_robot_get_device("motor_right");
+  WbDeviceTag motor_l = wb_robot_get_device("motor_left");
+  
+  wb_motor_set_position(motor_r, INFINITY);
+  wb_motor_set_position(motor_l, INFINITY);
+
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  while (wb_robot_step(TIME_STEP) != -1) {
+
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_differential_wheels_set_speed(100.0,100.0);
+     */
+    wb_motor_set_velocity(motor_r, -5);
+    wb_motor_set_velocity(motor_l, -5);
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
